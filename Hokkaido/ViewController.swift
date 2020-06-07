7c0975257744e734f4dfb4e5c21ddd1cd271f23f
tkm1202 <tkm1202@users.noreply.github.com>
2020-06-08 01:16:39 +0900

    add Podfile

--- Hokkaido/ViewController.swift
+++ Hokkaido/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //imageView作成
+        self.imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        //中央寄せ
+        self.imageView.center = self.view.center
+        //画像を設定
+        self.imageView.image = UIImage(named: "hrog")
+        //viewに追加
+        self.view.addSubview(self.imageView)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //スプラッシュ画面の実装（画像の縮小）
+        UIView.animate(withDuration: 0.3,
+                       delay: 1.0,
+                       options: UIView.AnimationOptions.curveEaseOut,
+                       animations: { () in
+                        self.imageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }, completion: { (Bool) in
+            
+        })
+        
+        //スプラッシュ画面の実装（画面の拡大）
+        UIView.animate(withDuration: 0.2,
+                       delay: 1.3,
+                       options: UIView.AnimationOptions.curveEaseOut,
+                       animations: { () in
+                        self.imageView.transform = CGAffineTransform(scaleX: 8.0, y: 8.0)
+                        self.imageView.alpha = 0
+        }, completion: { (Bool) in
+            //で、アニメーションが終わったらimageViewを消す
+            self.imageView.removeFromSuperview()
+        })
+    }
 
 }
 
