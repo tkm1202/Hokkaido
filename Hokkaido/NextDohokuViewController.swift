431060f3cb1c51f77561ddfafe291f665e0bb751
tkm1202 <tkm1202@users.noreply.github.com>
2020-07-14 05:26:31 +0900

    complete nextDohokuViewController

--- Hokkaido/NextDohokuViewController.swift
+++ Hokkaido/NextDohokuViewController.swift
@@ -35,26 +35,32 @@ class NextDohokuViewController: UIViewController {
         if areaNumber == 0{
             var dohokuArray = UserDefaults.standard.array(forKey: "Dohoku")
             dohokuArray?[cityNumber] = 0
+            print(dohokuArray ?? "error")
             UserDefaults.standard.set(dohokuArray,forKey: "Dohoku")
         }else if areaNumber == 1{
-            var dohokuArray = UserDefaults.standard.array(forKey: "Doto")
-            dohokuArray?[cityNumber] = 0
-            UserDefaults.standard.set(dohokuArray,forKey: "Doto")
+            var dotoArray = UserDefaults.standard.array(forKey: "Doto")
+            dotoArray?[cityNumber] = 0
+            UserDefaults.standard.set(dotoArray,forKey: "Doto")
+            print(dotoArray ?? "error")
         }else if areaNumber == 2{
-        var dohokuArray = UserDefaults.standard.array(forKey: "Tokachi")
-        dohokuArray?[cityNumber] = 0
-        UserDefaults.standard.set(dohokuArray,forKey: "Tokachi")
+        var tokachiArray = UserDefaults.standard.array(forKey: "Tokachi")
+        tokachiArray?[cityNumber] = 0
+        UserDefaults.standard.set(tokachiArray,forKey: "Tokachi")
+            print(tokachiArray ?? "error")
         }else if areaNumber == 3{
-        var dohokuArray = UserDefaults.standard.array(forKey: "Douou")
-        dohokuArray?[cityNumber] = 0
-        UserDefaults.standard.set(dohokuArray,forKey: "Douou")
+        var dououArray = UserDefaults.standard.array(forKey: "Douou")
+        dououArray?[cityNumber] = 0
+        UserDefaults.standard.set(dououArray,forKey: "Douou")
+            print(dououArray ?? "error")
         }else if areaNumber == 4{
-        var dohokuArray = UserDefaults.standard.array(forKey: "Donan")
-        dohokuArray?[cityNumber] = 0
-        UserDefaults.standard.set(dohokuArray,forKey: "Donan")
+        var donanArray = UserDefaults.standard.array(forKey: "Donan")
+        donanArray?[cityNumber] = 0
+        UserDefaults.standard.set(donanArray,forKey: "Donan")
+            print(donanArray ?? "error")
         }
         //画面戻る
-        self.dismiss(animated: true, completion: nil)
+        self.navigationController?.popViewController(animated: true)
+        
     }
     
     
@@ -63,25 +69,31 @@ class NextDohokuViewController: UIViewController {
                    var dohokuArray = UserDefaults.standard.array(forKey: "Dohoku")
                    dohokuArray?[cityNumber] = 1
                    UserDefaults.standard.set(dohokuArray,forKey: "Dohoku")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 1{
                    var dohokuArray = UserDefaults.standard.array(forKey: "Doto")
                    dohokuArray?[cityNumber] = 1
                    UserDefaults.standard.set(dohokuArray,forKey: "Doto")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 2{
                var dohokuArray = UserDefaults.standard.array(forKey: "Tokachi")
                dohokuArray?[cityNumber] = 1
                UserDefaults.standard.set(dohokuArray,forKey: "Tokachi")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 3{
                var dohokuArray = UserDefaults.standard.array(forKey: "Douou")
                dohokuArray?[cityNumber] = 1
                UserDefaults.standard.set(dohokuArray,forKey: "Douou")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 4{
                var dohokuArray = UserDefaults.standard.array(forKey: "Donan")
                dohokuArray?[cityNumber] = 1
                UserDefaults.standard.set(dohokuArray,forKey: "Donan")
+            print(dohokuArray ?? "error")
                }
         //画面戻る
-        self.dismiss(animated: true, completion: nil)
+         self.navigationController?.popViewController(animated: true)
+       
     }
     
     
@@ -90,25 +102,30 @@ class NextDohokuViewController: UIViewController {
                    var dohokuArray = UserDefaults.standard.array(forKey: "Dohoku")
                    dohokuArray?[cityNumber] = 2
                    UserDefaults.standard.set(dohokuArray,forKey: "Dohoku")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 1{
                    var dohokuArray = UserDefaults.standard.array(forKey: "Doto")
                    dohokuArray?[cityNumber] = 2
                    UserDefaults.standard.set(dohokuArray,forKey: "Doto")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 2{
                var dohokuArray = UserDefaults.standard.array(forKey: "Tokachi")
                dohokuArray?[cityNumber] = 2
                UserDefaults.standard.set(dohokuArray,forKey: "Tokachi")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 3{
                var dohokuArray = UserDefaults.standard.array(forKey: "Douou")
                dohokuArray?[cityNumber] = 2
                UserDefaults.standard.set(dohokuArray,forKey: "Douou")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 4{
                var dohokuArray = UserDefaults.standard.array(forKey: "Donan")
                dohokuArray?[cityNumber] = 2
                UserDefaults.standard.set(dohokuArray,forKey: "Donan")
+            print(dohokuArray ?? "error")
                }
         //画面戻る
-        self.dismiss(animated: true, completion: nil)
+         self.navigationController?.popViewController(animated: true)
     }
     
     
@@ -117,25 +134,30 @@ class NextDohokuViewController: UIViewController {
                    var dohokuArray = UserDefaults.standard.array(forKey: "Dohoku")
                    dohokuArray?[cityNumber] = 3
                    UserDefaults.standard.set(dohokuArray,forKey: "Dohoku")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 1{
                    var dohokuArray = UserDefaults.standard.array(forKey: "Doto")
                    dohokuArray?[cityNumber] = 3
                    UserDefaults.standard.set(dohokuArray,forKey: "Doto")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 2{
                var dohokuArray = UserDefaults.standard.array(forKey: "Tokachi")
                dohokuArray?[cityNumber] = 3
                UserDefaults.standard.set(dohokuArray,forKey: "Tokachi")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 3{
                var dohokuArray = UserDefaults.standard.array(forKey: "Douou")
                dohokuArray?[cityNumber] = 3
                UserDefaults.standard.set(dohokuArray,forKey: "Douou")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 4{
                var dohokuArray = UserDefaults.standard.array(forKey: "Donan")
                dohokuArray?[cityNumber] = 3
                UserDefaults.standard.set(dohokuArray,forKey: "Donan")
+            print(dohokuArray ?? "error")
                }
         //画面戻る
-        self.dismiss(animated: true, completion: nil)
+        self.navigationController?.popViewController(animated: true)
     }
     
     
@@ -144,24 +166,29 @@ class NextDohokuViewController: UIViewController {
                    var dohokuArray = UserDefaults.standard.array(forKey: "Dohoku")
                    dohokuArray?[cityNumber] = 4
                    UserDefaults.standard.set(dohokuArray,forKey: "Dohoku")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 1{
                    var dohokuArray = UserDefaults.standard.array(forKey: "Doto")
                    dohokuArray?[cityNumber] = 4
                    UserDefaults.standard.set(dohokuArray,forKey: "Doto")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 2{
                var dohokuArray = UserDefaults.standard.array(forKey: "Tokachi")
                dohokuArray?[cityNumber] = 4
                UserDefaults.standard.set(dohokuArray,forKey: "Tokachi")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 3{
                var dohokuArray = UserDefaults.standard.array(forKey: "Douou")
                dohokuArray?[cityNumber] = 4
                UserDefaults.standard.set(dohokuArray,forKey: "Douou")
+            print(dohokuArray ?? "error")
                }else if areaNumber == 4{
                var dohokuArray = UserDefaults.standard.array(forKey: "Donan")
                dohokuArray?[cityNumber] = 4
                UserDefaults.standard.set(dohokuArray,forKey: "Donan")
+            print(dohokuArray ?? "error")
                }
         //画面戻る
-        self.dismiss(animated: true, completion: nil)
+        self.navigationController?.popViewController(animated: true)
     }
 }
