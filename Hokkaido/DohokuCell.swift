bca3d06c5dbfcee8c16eea54c7046ace1c562108
tkm1202 <tkm1202@users.noreply.github.com>
2020-07-14 04:01:07 +0900

    add buttons

---
+++ Hokkaido/DohokuCell.swift
@@ -0,0 +1,17 @@
+//
+//  DohokuCell.swift
+//  Hokkaido
+//
+//  Created by takumi on 2020/07/14.
+//  Copyright © 2020 TakumiKato. All rights reserved.
+//
+
+import UIKit
+
+class DohokuCell:UITableViewCell {
+
+    @IBOutlet weak var regionLabel:UILabel!
+    @IBOutlet weak var colorLabel:UILabel!
+ 
+
+}
--- Hokkaido/DohokuViewController.swift
+++ Hokkaido/DohokuViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
  
 class DohokuViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-   
     
-    let textArray = ["稚内","名寄","留萌","小樽","久遠","函館","室蘭","札幌","苫小牧","襟裳","帯広","旭川","",""]
+    
+    // section毎のラベルのarray
+    let DohokuArray: NSArray = ["稚内","名寄","留萌"]
+    let DotoArray: NSArray = ["知床"]
+    let TokachiArray: NSArray = ["苫小牧","襟裳","帯広"]
+    let DououArray: NSArray = ["室蘭","札幌","旭川"]
+    let DonanArray: NSArray = ["小樽","久遠","函館"]
+    
+    //let textArray = []
     private let mySections: NSArray = ["道北", "道東","十勝","道央","道南"]
     
     @IBOutlet weak var dohokuTableView: UITableView!
@@ -45,22 +52,73 @@ class DohokuViewController: UIViewController,UITableViewDelegate,UITableViewData
         
         let nextVC = storyboard?.instantiateViewController(identifier: "toNextDohoku") as! NextDohokuViewController
         
-        nextVC.cityString = textArray[indexPath.row]
+        if indexPath.section == 0{
+            nextVC.cityString = DohokuArray[indexPath.row] as! String
+        }else if indexPath.section == 1{
+            nextVC.cityString = DotoArray[indexPath.row] as! String
+        }else if indexPath.section == 2{
+            nextVC.cityString = TokachiArray[indexPath.row] as! String
+        }else if indexPath.section == 3{
+            nextVC.cityString = DououArray[indexPath.row] as! String
+        }else if indexPath.section == 4{
+            nextVC.cityString = DonanArray[indexPath.row] as! String
+        }
+        
         navigationController?.pushViewController(nextVC, animated: true)
         
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return textArray.count
+        if section == 0 {
+            return DohokuArray.count
+        }else if section == 1 {
+            return DotoArray.count
+        }else if section == 2{
+            return TokachiArray.count
+        }else if section == 3{
+            return DououArray.count
+        }else if section == 4{
+            return DonanArray.count
+        }
+        return 0
        }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         if let cell = dohokuTableView.dequeueReusableCell(withIdentifier: "DohokuCell") as? DohokuCell {
+            
+            if indexPath.section == 0 {
+                       print("indexPath.section == 0")
+            
+                let label = cell.regionLabel!
+                       label.text = String(describing: DohokuArray[indexPath.row])
+                   }
+                   else if indexPath.section == 1 {
+                       print("indexPath.section == 1")
+                       let label = cell.regionLabel!
+                       label.text = String(describing: DotoArray[indexPath.row])
+                   }
+                   else if indexPath.section == 2 {
+                       print("indexPath.section == 2")
+                      let label = cell.regionLabel!
+                      label.text = String(describing: TokachiArray[indexPath.row])
+                   }
+            else if indexPath.section == 3 {
+                print("indexPath.section == 3")
+               let label = cell.regionLabel!
+               label.text = String(describing: DououArray[indexPath.row])
+            }
+            else if indexPath.section == 4 {
+                print("indexPath.section == 4")
+               let label = cell.regionLabel!
+               label.text = String(describing: DonanArray[indexPath.row])
+            }
+           // cell.regionLabel!.text = textArray[indexPath.row]
+             return cell
+         }
+         return UITableViewCell()
+    }
 
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = dohokuTableView.dequeueReusableCell(withIdentifier: "DohokuCell", for: indexPath)
-        cell.textLabel?.text = textArray[indexPath.row]
-        //cell.imageView?.image = UIImage(named: "checkImage")
-           return cell
-       }
+      
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return view.frame.size.height/10
